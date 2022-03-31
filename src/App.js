35fa654f0070dc74e87b423cd7d432bcde89b495
samsundar <samradnus2001@gmail.com>
2022-03-31 07:31:27 +0530

    looping components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Canvas from './components/Canvas';
+import PropTypes from 'prop-types';
 function App() {
+  var rows=[];
+  
+  for(var i=0;i<10;i++){
+    rows.push(<Canvas row={i}/>)
+  }
   return (
-    <div className="App">
-        <Canvas/>
-    </div>
+    <>
+      <button type="button" class="btn btn-outline-dark">Dark</button>
+      <tbody>{rows}</tbody>
+      </>
   );
 }
 
