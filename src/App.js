37d67e347e85e979068353d6fe4e803295974592
samsundar <samradnus2001@gmail.com>
2022-03-31 08:28:08 +0530

    Added re-render component on click

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Canvas from './components/Canvas';
 import PropTypes from 'prop-types';
+import { Component, useState } from 'react';
+function useForceUpdate(){
+  const [value,setValue]=useState(0);
+  return()=>setValue(value=>Math.random());
+}
+
+function generateNum(){
+  var x=document.getElementById('number');
+  x.innerHTML=Math.random();
+}
 function App() {
   var rows=[];
-  
+  const forceUpdate=useForceUpdate();
   for(var i=0;i<10;i++){
     rows.push(<Canvas row={i}/>)
   }
   return (
     <>
-      <button type="button" class="btn btn-outline-dark">Dark</button>
-      <tbody>{rows}</tbody>
+      <div>
+      <button type="button" onClick={forceUpdate} className="btn btn-outline-dark mx-5 my-5">Dark</button>
+     <p id="number">{Math.random()}</p>
+     
+     </div>
+      <tbody onClick={generateNum}>{rows}</tbody>
       </>
   );
 }
