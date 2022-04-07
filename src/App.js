0518b1240e218b605e57729faf567dc983da2303
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-04-07 16:28:31 +0530

    Added Map Movies

--- src/App.js
+++ src/App.js
@@ -2,47 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import Canvas from './components/Canvas';
 import PropTypes from 'prop-types';
+import MovieList from './components/MovieList';
 import { Component, useState } from 'react';
-function useForceUpdate(){
-  const [value,setValue]=useState(0);
-  return()=>setValue(value=>Math.random());
-}
+export const App = () => {
+  const [movies, setMovies] = useState([]);
+  var rows = [];
+  async function fetchMovie() {
+    // const url='https://api.themoviedb.org/3/search/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-UK&query=Batman&page=1&region=GB';
+    const url = 'https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&primary_release_date.gte=2021-01-01&primary_release_date.lte=2022-01-01&language=en-US&page=1';
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    setMovies(parsedData.results);
+    //... spread attributes to make passing of results easiers
+    console.log(movies);
 
-function generateNum(){
-  var x=document.getElementById('number');
-  x.innerHTML=Math.random();
-}
-async function fetchFalsey()
-{
-  const url='https://newsapi.org/v2/top-headlines?q=UK&apiKey=2902e07919cb4e268ec388cc3f930a7e';
-  let data=await fetch(url);
-  console.log(!!document.getElementById('falsey'));
-  console.log(document.getElementById('falsey'));
-}
-async function fetchMovie()
-{
- // const url='https://api.themoviedb.org/3/search/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-UK&query=Batman&page=1&region=GB';
-  const url='https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&primary_release_date.gte=2021-01-01&primary_release_date.lte=2022-01-01&language=en-US&page=1';
-  let data=await fetch(url);
-  let parsedData=await data.json();
-  console.log(parsedData);
-}
-function App() {
-  var rows=[];
-  const forceUpdate=useForceUpdate();
-  for(var i=0;i<10;i++){
-    rows.push(<Canvas row={i}/>)
   }
+
+
+
   return (
     <>
-      <div>
-      <button type="button" onClick={forceUpdate} className="btn btn-outline-dark mx-5 my-5">Dark</button>
-     <p id="number">{Math.random()}</p>
-     
-     </div>
-      <tbody onClick={fetchMovie}>{rows}</tbody>
 
-      </>
+      <button className="btn btn-primary mx-5 my-5" onClick={fetchMovie}>click me</button>
+      <div className='container'>
+        <div className="row row-cols-4">
+          {movies.map(movie => (
+
+            <MovieList key={movie.id} imageURL={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} title={movie.title} details={movie.overview.slice(0,50)} date={movie.release_date} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
