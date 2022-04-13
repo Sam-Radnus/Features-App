15dda43e0439baf49a40a24336031688798ce86a
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-04-13 09:21:19 +0530

    Updated useEffect

--- src/App.js
+++ src/App.js
@@ -4,33 +4,80 @@ import Canvas from './components/Canvas';
 import PropTypes from 'prop-types';
 import MovieList from './components/MovieList';
 import { Component, useState } from 'react';
+import { useEffect } from 'react';
 export const App = () => {
-  const [movies, setMovies] = useState([]);
-  var rows = [];
-  async function fetchMovie() {
-    // const url='https://api.themoviedb.org/3/search/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-UK&query=Batman&page=1&region=GB';
-    const url = 'https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&primary_release_date.gte=2021-01-01&primary_release_date.lte=2022-01-01&language=en-US&page=1';
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    setMovies(parsedData.results);
-    //... spread attributes to make passing of results easiers
-    console.log(movies);
+  const [movieName1, setMovieName1] = useState('');
+  const [genreAR1, setGenre1] = useState([]);
+  const [genreAR2, setGenre2] = useState([]);
+  const [genreAR3, setGenre3]=  useState([]);
+  const [commonGenre, setCommon] = useState([]);
+  const [movieName2, setMovieName2] = useState('28');
 
+  useEffect(() => {
+    asyncData();
+  }, [genreAR3.length])
+  const asyncData = async () => {
+
+    const url1 = 'https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=2022-12-31&vote_average.gte=6&with_genres=53';
+    const data1 = await fetch(url1);
+    if (data1.length !== 0) {
+      const parsedData1 = await data1.json();
+      let x = parsedData1.results[0].genre_ids;
+
+      setGenre1(x);
+      console.log(genreAR1);
+      setMovieName1(parsedData1.results[0].title);
+    }
+    else {
+      console.log("Failure");
+    }
+
+    const url2 = 'https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=2022-12-31&vote_average.gte=6&with_genres=80';
+    const data2 = await fetch(url2);
+    if (data2.length !== 0) {
+      const parsedData2 = await data2.json();
+
+      let y = parsedData2.results[1].genre_ids;
+      console.log(y);
+      setGenre2(y,findCommon());
+      console.log(genreAR2);
+      setMovieName2(parsedData2.results[1].title);
+    }
+    else {
+      console.log("Failure");
+    }
+    findCommon();
+    console.log(commonGenre);
   }
 
-
-
+  const findCommon = async () => {
+    var data = [genreAR1, genreAR2];
+    let result = data.reduce((a, b) => a.filter(c => b.includes(c)));
+    let search = JSON.stringify(result);
+    console.log(search);
+    console.log(search.slice(1, search.length - 1));
+    setGenre3(search.slice(1, search.length - 1));
+    console.log(genreAR3);
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&with_genres=${search.slice(1, search.length - 1)}`
+    const common = await fetch(url);
+    if (common.length !== 0) {
+      const parsedData = await common.json();
+      console.log(parsedData.results);
+      setCommon(parsedData.results);
+      console.log(commonGenre)
+    }
+    else {
+      console.log("Failure");
+    }
+  }
   return (
     <>
 
-      <button className="btn btn-primary mx-5 my-5" onClick={fetchMovie}>click me</button>
+      <button className="btn btn-primary mx-5 my-5">click me</button>
       <div className='container'>
         <div className="row row-cols-4">
-          {movies.map(movie => (
+          <h1></h1>
 
-            <MovieList key={movie.id} imageURL={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} title={movie.title} details={movie.overview.slice(0,50)} date={movie.release_date} />
-          ))}
         </div>
       </div>
     </>
