fcda3d933f9b465618c7c1af715bfde17b627cc4
samsundar <samradnus2001@gmail.com>
2022-05-01 12:58:36 +0530

    Bug Fixes

---
+++ src/components/LoginContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function LoginContext(props) {
+  return (
+    <div>LoginContext</div>
+  )
+}
+
+LoginContext.propTypes = {}
+
+export default LoginContext
